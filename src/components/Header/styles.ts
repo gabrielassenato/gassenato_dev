7ac720e9eb82deccbda7bb259f1eb4178217b866
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-22 18:08:09 -0300

    :sparkles: feat(header): Adicionando e posicionando os itens

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import {colors} from '../../styles'
+
+export const HeaderBar = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 24px 0;
+`
+
+export const Logo = styled.h1`
+  font-weight: 900;
+`
+
+export const LinkList = styled.ul`
+  display: flex;
+`
+
+export const LinkItens = styled.li`
+  margin-left: 40px;
+
+  :hover {
+      color: ${colors.green};
+    }
+
+  a {
+    color: ${colors.white};
+  }
+`
