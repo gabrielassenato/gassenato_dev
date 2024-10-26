53d132615b6cd00788c983d51635df4cb1103fca
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 16:16:48 -0300

    :sparkles: feat(hero): Adicionando a seção hero

---
+++ src/containers/Hero/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "../../styles"
+import { Button, HeroContainer } from "./styles"
+
+const Hero = () => (
+  <HeroContainer>
+    <Container>
+      <h1>
+        Desenvolvo soluções digitais <br />
+        modernas que impulsionam <br />
+        o sucesso da sua empresa.
+      </h1>
+      <h4>Desenvolvedor Full-Stack.</h4>
+      <Button>Entre em contato</Button>
+    </Container>
+  </HeroContainer>
+)
+
+export default Hero
