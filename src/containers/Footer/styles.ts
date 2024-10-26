d990ba1e67fb65fad54e0bc426b87f29c9c2639e
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 16:19:44 -0300

    :sparkles: feat(header): Adicionando a seção header

---
+++ src/containers/Footer/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+export const FooterBar = styled.div`
+  background-color: ${colors.lightGrey};
+  color: #000;
+  margin-top: 42px;
+  text-align: center;
+  padding: 18px;
+`
