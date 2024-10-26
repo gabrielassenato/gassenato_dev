74c2b5c1772b584671257884177662145a0f9e98
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 16:16:11 -0300

    :sparkles: feat(about): Adicionando a seção about

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,15 @@
+import { AboutContainer, Avatar, TextAbout } from "./styles"
+import avatar from '../../assets/images/sobre.png'
+
+const About = () => (
+  <AboutContainer>
+    <TextAbout>
+      <h1>Sobre mim</h1>
+      <p>Meu nome é Gabriel, sou formado em Análise e Desenvolvimento de Sistemas. Durante minha jornada,  adquiri sólidos conhecimentos em REACT e JavaScript, além de aprender a utilizar SASS, Vue, Grunt e outras tecnologias. Estou sempre em busca de aprimorar minhas habilidades e me manter atualizado com as tendências do mercado de desenvolvimento.</p>
+    </TextAbout>
+    <Avatar>
+    </Avatar>
+  </AboutContainer>
+)
+
+export default About
