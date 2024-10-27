45048a80c3de768aba17f2e0d4d75158ebe34567
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 23:52:07 -0300

    :lipstick: feat(globalStyle): Estilização global

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components'
+import background from '../assets/images/background.jpg'
 
 export const colors = {
   green: '#60B98C',
@@ -9,18 +10,29 @@ export const colors = {
   lightBlue: '#B0BEC5'
 }
 
+export const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+`;
+
 export const GlobalStyle = createGlobalStyle`
     *{
       margin: 0;
       padding: 0;
       box-sizing: border-box;
       font-family: "Montserrat", serif;
-      background-color: black;
     }
 
     body {
-      background-color: ${colors.black};
       color: ${colors.white};
+      background-color: ${colors.black};
+      background-image:
+        linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.6)),
+        url(${background});
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      }
 
       li {
         list-style: none;
@@ -29,5 +41,4 @@ export const GlobalStyle = createGlobalStyle`
       a {
         text-decoration: none;
       }
-    }
 `
