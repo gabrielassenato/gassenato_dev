01be4737c3e4d60877b0088d77847d158dca5c1e
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 23:54:47 -0300

    :refactor: refactor: Refatorando a forma de apresentar o avatar

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -8,6 +8,7 @@ const About = () => (
       <p>Meu nome é Gabriel, sou formado em Análise e Desenvolvimento de Sistemas. Durante minha jornada,  adquiri sólidos conhecimentos em REACT e JavaScript, além de aprender a utilizar SASS, Vue, Grunt e outras tecnologias. Estou sempre em busca de aprimorar minhas habilidades e me manter atualizado com as tendências do mercado de desenvolvimento.</p>
     </TextAbout>
     <Avatar>
+      <img src={avatar} alt="" />
     </Avatar>
   </AboutContainer>
 )
