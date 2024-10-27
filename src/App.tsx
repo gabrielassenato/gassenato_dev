f23310fe8c7e1edd8dd74bdd4d46016ee9202383
Gabriel Assenato <gabrielassenato@gmail.com>
2024-10-26 23:55:24 -0300

    :sparkles: feat(app): Importando os containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { GlobalStyle } from './styles'
-import Header from './components/Header'
+import { Container, GlobalStyle } from './styles'
+import Header from './containers/Header'
+import Hero from './containers/Hero'
+import About from './containers/About'
+import MyProjects from './containers/MyProjects'
+import Skills from './containers/Skills'
+import { Contact } from './containers/Contact'
+import { Footer } from './containers/Footer'
+import background from '../../assets/images/background.jpg';
 
 function App() {
   return (
     <>
     <GlobalStyle />
-      <Header />
+      <Container>
+        <Header />
+      </Container>
+        <Hero />
+      <Container>
+        <About />
+        <MyProjects />
+        <Skills />
+        <Contact />
+      </Container>
+      <Footer />
     </>
   )
 }
