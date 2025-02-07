9226e71f72b8fbd753189359255a9570b442314d
Gabriel Assenato <gabrielassenato@gmail.com>
2025-02-07 00:03:51 -0300

    :lipstick: feat: Melhorando o layout da seção sobre

--- src/containers/About/styles.ts
+++ src/containers/About/styles.ts
@@ -2,25 +2,40 @@ import styled from "styled-components";
 import { colors } from "../../styles";
 
 export const AboutContainer = styled.div`
-  display: grid;
-  grid-template-columns: 55% 40%;
-  gap: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
   background-color: ${colors.black};
   border: 1px solid ${colors.green};
   border-radius: 10px;
-  padding: 62px;
-`
+  padding: 40px;
+  gap: 20px;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 55% 40%;
+    text-align: left;
+    gap: 32px;
+    padding: 62px;
+  }
+`;
 
 export const TextAbout = styled.div`
   h1 {
-    margin-bottom: 26px;
+    margin-bottom: 20px;
+    font-size: 24px;
   }
-`
+  p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+`;
 
 export const Avatar = styled.div`
-  align-items: center;
-
-  img{
-    width: 90%;
+  img {
+    width: 100%;
+    max-width: 300px;
+    border-radius: 10px;
   }
-`
+`;
