ca50080594fc0fb0adf47c1527fc9cc9182b7059
Gabriel Assenato <gabrielassenato@gmail.com>
2025-02-06 23:59:18 -0300

    :broom: cleanup: Limpando importações não utilizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Container, GlobalStyle } from './styles'
 import Header from './containers/Header'
 import Hero from './containers/Hero'
@@ -9,7 +6,6 @@ import MyProjects from './containers/MyProjects'
 import Skills from './containers/Skills'
 import { Contact } from './containers/Contact'
 import { Footer } from './containers/Footer'
-import background from '../../assets/images/background.jpg';
 
 function App() {
   return (
