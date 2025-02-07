722a0142b804db65e6b758deca92fbb78265cb22
Gabriel Assenato <gabrielassenato@gmail.com>
2025-02-07 00:00:16 -0300

    :sparkles: feat: Menu hamburguer

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,25 +1,42 @@
-import { HeaderBar, LinkItens, LinkList, Logo } from "./styles"
+import { useState } from 'react'
+import {
+  HeaderBar,
+  LinkItens,
+  LinkList,
+  Logo,
+  MenuButton,
+  NavContainer
+} from './styles'
 
-const Header = () => (
-  <HeaderBar>
-    <Logo>GASSENATO</Logo>
-    <nav>
-      <LinkList>
-        <LinkItens>
-          <a href="#about">Sobre</a>
-        </LinkItens>
-        <LinkItens>
-          <a href="#about">Projetos</a>
-        </LinkItens>
-        <LinkItens>
-          <a href="#about">Skills</a>
-        </LinkItens>
-        <LinkItens>
-          <a href="#about">Contato</a>
-        </LinkItens>
-      </LinkList>
-    </nav>
-  </HeaderBar>
-)
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <HeaderBar>
+      <Logo>GASSENATO</Logo>
+      <MenuButton onClick={() => setMenuOpen(!menuOpen)}>
+        <span />
+        <span />
+        <span />
+      </MenuButton>
+      <NavContainer menuOpen={menuOpen}>
+        <LinkList>
+          <LinkItens>
+            <a href="#about">Sobre</a>
+          </LinkItens>
+          <LinkItens>
+            <a href="#about">Projetos</a>
+          </LinkItens>
+          <LinkItens>
+            <a href="#about">Skills</a>
+          </LinkItens>
+          <LinkItens>
+            <a href="#about">Contato</a>
+          </LinkItens>
+        </LinkList>
+      </NavContainer>
+    </HeaderBar>
+  )
+}
 
 export default Header
--- src/containers/Header/styles.ts
+++ src/containers/Header/styles.ts
@@ -7,25 +7,64 @@ export const HeaderBar = styled.header`
   justify-content: space-between;
   margin: 0 auto;
   padding: 24px 0;
+  position: relative;
 `
 
 export const Logo = styled.h1`
   font-weight: 900;
 `
 
+export const MenuButton = styled.button`
+  width: 32px;
+
+  span {
+    height: 4px;
+    display: block;
+    width: 100%;
+    background-color: #000;
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavContainer = styled.nav<{ menuOpen: boolean }>`
+  @media (max-width: 768px) {
+    position: absolute;
+    top: 60px;
+    right: 0;
+    background-color: ${colors.black};
+    width: 200px;
+    display: ${(props) => (props.menuOpen ? "block" : "none")};
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+  }
+`;
+
 export const LinkList = styled.ul`
   display: flex;
-`
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 10px;
+  }
+`;
 
 export const LinkItens = styled.li`
   margin-left: 40px;
 
   :hover {
-      color: ${colors.green};
-    }
+    color: ${colors.green};
+  }
 
   a {
     color: ${colors.white};
     font-family: "Poppins", serif;
   }
-`
+
+  @media (max-width: 768px) {
+    margin: 10px 0;
+  }
+`;
